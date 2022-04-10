6c6a4a0bde945864091d1fb2404329b1a2e9d39d
takahiro-it <tk.ryu.07@gmail.com>
2022-04-10 19:30:25 +0900

    テストコードの修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,8 +1,5 @@
 FactoryBot.define do
   factory :item do
-    
-    after(:build) do |item|
-    @item.image = fixture_file_upload('public/images/test_image.png')
 
     name { Faker::Commerce.product_name }
     description { Faker::Lorem.characters(16) }
@@ -13,6 +10,10 @@ FactoryBot.define do
     shopping_day_id { Faker::Number.within(range: 1..3) }
     price { Faker::Number.within(range: 300..9_999_999) }
     association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+
   end
 end
 end
