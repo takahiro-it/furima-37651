3cbb04dec2ebc0e660ee157af208f3ecef03bb44
takahiro-it <tk.ryu.07@gmail.com>
2022-04-11 08:45:45 +0900

    ブランク部分のコード追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,9 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
-  # before_action :set_item, only: [:show, :edit, :update, :destroy,]
+  before_action :set_item, only: [:show, :edit, :update, :destroy,]
 
   def index
-    # @item = Item.all
+    @item = Item.all
   end
 
   def new
@@ -12,13 +12,32 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    
     if @item.save
       redirect_to root_path
     else
       render :new
     end 
-  end   
+  end 
+  
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @item.destroy
+      redirect_to root_path
+    else
+      redirect :show
+    end
+  end
 
   private
 
@@ -26,9 +45,10 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :price, :description, :category_id, :user_id, :status_id, :shopping_cost_id, :shopping_day_id, :prefecture_id, :image ).merge(user_id: current_user.id)
   end
 
-  # def set_item
-  #   @item = Item.find(params[:id])
-  # end
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
 
   
