e1f3b9a03bc97a7049b588599b79bc8ac437d9f0
takahiro-it <tk.ryu.07@gmail.com>
2022-04-11 15:08:19 +0900

    コードレビュー後修正2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root "items#index"
-  get 'items'     => 'items#index'
   resources :items do
     resources :comments, only: [:create, :destroy, :show, :index] 
   end
