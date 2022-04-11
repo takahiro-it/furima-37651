4105580df2978ff815b7f9af71875949164c97cc
takahiro-it <tk.ryu.07@gmail.com>
2022-04-11 11:31:19 +0900

    商品一覧表示確認

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root "items#index"
+  get 'items'     => 'items#index'
   resources :items do
-    resources :comments, only: [:create, :destroy, :show] 
+    resources :comments, only: [:create, :destroy, :show, :index] 
   end
 end
