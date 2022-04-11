e30e1e24949909c81e988216a7e22e4eb80c86aa
takahiro-it <tk.ryu.07@gmail.com>
2022-04-11 18:22:33 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "items#index"
   resources :items do
-    resources :items, only: [:create, :destroy, :show, :index] 
+    resources :comments, only: [:create, :destroy, :show, :index] 
   end
 end
