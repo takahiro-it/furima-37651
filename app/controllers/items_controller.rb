3b2b4608f283bfee7603e656fe378dbd49935499
takahiro-it <tk.ryu.07@gmail.com>
2022-04-11 14:33:36 +0900

    コードレビュー後修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
-  before_action :set_item, only: [:show, :edit, :update, :destroy,]
+  # before_action :set_item, only: [:show, :edit, :update, :destroy,]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -19,25 +19,25 @@ class ItemsController < ApplicationController
     end 
   end 
   
-  def show
-    @item = Item.find(params[:id])
-  end
+  # def show
+  #   @item = Item.find(params[:id])
+  # end
 
-  def update
-    if @item.update(item_params)
-      redirect_to item_path(@item.id)
-    else
-      render :edit
-    end
-  end
+  # def update
+  #   if @item.update(item_params)
+  #     redirect_to item_path(@item.id)
+  #   else
+  #     render :edit
+  #   end
+  # end
 
-  def destroy
-    if @item.destroy
-      redirect_to root_path
-    else
-      redirect :show
-    end
-  end
+  # def destroy
+  #   if @item.destroy
+  #     redirect_to root_path
+  #   else
+  #     redirect :show
+  #   end
+  # end
 
   private
 
@@ -45,9 +45,9 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :price, :description, :category_id, :user_id, :status_id, :shopping_cost_id, :shopping_day_id, :prefecture_id, :image ).merge(user_id: current_user.id)
   end
 
-  def set_item
-    @item = Item.find(params[:id])
-  end
+  # def set_item
+  #   @item = Item.find(params[:id])
+  # end
 
 end
 
