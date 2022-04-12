2e4531bf6ee4b10704767e42200d10073c821360
takahiro-it <tk.ryu.07@gmail.com>
2022-04-12 22:13:58 +0900

    コントローラー追記

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,16 +1,28 @@
 class OrdersController < ApplicationController
   def index
-    #フォームオブジェクトのインスタンスを生成し、インスタンス変数に代入する
+    @item = Item.find(params[:item_id])
     @order_customer = OrderCustomer.new
   end
 
   def create
-    @order_customer = OrderCustomer.new(order_params)
+    @item = Item.find(params[:item_id])
+    @order_customer = OrderCustomer.new(ordere_params)
+    if @order_customer.valid?
+      @order_customer.save
+      redirect_to root_path 
+    else
+      render :index
+    end
+  end
+
+  def new
+    @order_customer = OrderCustomer.new
   end
 
   private
   def order_params
-    params.require(:order_customer).permit(各カラム名).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_customer).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :price).merge(user_id: current_user.id, item_id: @item.id)
+  end
   end
 
 end
--- app/models/order_customer.rb
+++ app/models/order_customer.rb
@@ -1,17 +1,23 @@
 class OrderCustomer < ApplicationRecord
   include ActiveModel::Model
-  atter_accessor :post_code, :prefecture, :city, :address, :building_name, :price, :user_id
+  atter_accessor :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :price, :user_id, :item_id
+
+  validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
   
   with_options presence: true do
     validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: 'is invalid'}
     validates :user_id
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :item_id
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :city
+    validates :address
+    validates :phone_number, format: {with: /\A[0-9]{11}\z/ }
   end
-  validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
-end
   
   def save
-    Order.create(カラム名
-    Customer.create(カラム名
-
+    order = Order.create(item_id: item_id, user_id: user_id)
+    Customer.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number, order_id: order.id)
+  end
 end
+
+
