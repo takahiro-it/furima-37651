e3d202399da72ee9538b0df201d1d89321c3b1f5
takahiro-it <tk.ryu.07@gmail.com>
2022-04-12 21:20:20 +0900

    Update order_customer.rb

--- app/models/order_customer.rb
+++ app/models/order_customer.rb
@@ -1,8 +1,14 @@
 class OrderCustomer < ApplicationRecord
   include ActiveModel::Model
-  atter_accessor :カラム名
+  atter_accessor :post_code, :prefecture, :city, :address, :building_name, :price, :user_id
   
-  validates
+  with_options presence: true do
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: 'is invalid'}
+    validates :user_id
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  end
+  validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
+end
   
   def save
     Order.create(カラム名
