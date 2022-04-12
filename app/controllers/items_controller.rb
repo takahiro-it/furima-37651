7eff8ca12397ae044e5dd773c3554fed8a5de697
takahiro-it <tk.ryu.07@gmail.com>
2022-04-12 11:58:04 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,7 +37,7 @@ class ItemsController < ApplicationController
 
   def destroy
     if @item.destroy
-      redirect_to root_path
+      redirect_to root_path unless current_user.id == @item.user_id
     else
       redirect :show
     end
