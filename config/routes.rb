df5a41cb8b017cdbfa4e63985de7b3b2337776fa
takahiro-it <tk.ryu.07@gmail.com>
2022-04-13 13:53:16 +0900

    テストコード追記

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: "items#index"
   # root to: 'orders#index'
   resources :items do
-    resources :orders, only: [:index, :create]
+    resources :orders, only: [:index, :create, :new]
   end
 end
