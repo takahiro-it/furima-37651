b70e0afbb6356b07cfdfe60b4e581d8227d2594f
takahiro-it <tk.ryu.07@gmail.com>
2022-04-13 13:55:20 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  # root to: 'orders#index'
   resources :items do
     resources :orders, only: [:index, :create, :new]
   end
