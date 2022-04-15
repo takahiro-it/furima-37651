f4e8a3412ba3ef9e52e4264c62ed1c001a6971d6
takahiro-it <tk.ryu.07@gmail.com>
2022-04-15 15:46:49 +0900

    購入でDB保存成功まで

--- app/models/order_customer.rb
+++ app/models/order_customer.rb
@@ -3,7 +3,6 @@ class OrderCustomer
   attr_accessor :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :price, :orders_id, :user_id, :item_id, :token
 
   with_options presence: true do
-    # validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: 'is invalid'}
     validates :user_id
     validates :item_id
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -16,8 +15,7 @@ class OrderCustomer
   
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    
-      Customer.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number, orders_id: order.id)
+      Customer.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -40,10 +40,10 @@ ActiveRecord::Schema.define(version: 2022_04_12_055801) do
     t.string "address", null: false
     t.string "building_name"
     t.string "phone_number", null: false
-    t.bigint "orders_id", null: false
+    t.bigint "order_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["orders_id"], name: "index_customers_on_orders_id"
+    t.index ["order_id"], name: "index_customers_on_order_id"
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -89,7 +89,7 @@ ActiveRecord::Schema.define(version: 2022_04_12_055801) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "customers", "orders", column: "orders_id"
+  add_foreign_key "customers", "orders"
   add_foreign_key "items", "users"
   add_foreign_key "orders", "items"
   add_foreign_key "orders", "users"
